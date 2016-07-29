556b33c7be4472d0888d145d926d63b8e9964d10
helpermethod <weiler@predic8.de>
2016-07-29 15:00:04 +0200

    Add license header

--- src/main/java/com/github/helpermethod/membrane/plugin/RunMojo.java
+++ src/main/java/com/github/helpermethod/membrane/plugin/RunMojo.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 predic8 GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.helpermethod.membrane.plugin;
 
 import org.apache.maven.plugin.AbstractMojo;
@@ -8,10 +23,10 @@ import org.apache.maven.plugins.annotations.Parameter;
 
 @Mojo(name = "run")
 public class RunMojo extends AbstractMojo {
-    @Parameter(property = "membrane.proxiesPath", defaultValue = "src/test/resources/proxies.xml")
-    private String proxiesPath;
+	@Parameter(property = "membrane.proxiesPath", defaultValue = "src/test/resources/proxies.xml")
+	private String proxiesPath;
 
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        RouterFacade.createStarted(proxiesPath).waitForFinish();
-    }
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		RouterFacade.createStarted(proxiesPath).waitForFinish();
+	}
 }
