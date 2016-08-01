ffda91ed422b4d0cf7a8dcac6fac17c6ad19b72a
helpermethod <weiler@predic8.de>
2016-08-01 10:09:04 +0200

    Fix license header

--- src/main/java/com/helpermethod/membrane/plugin/RunMojo.java
+++ src/main/java/com/helpermethod/membrane/plugin/RunMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 predic8 GmbH
+ * Copyright 2016 helpermethod
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
