b8034af9776f8dabf1f5c39b79275a6e89c7dfee
Oliver Weiler <wo6980@gmail.com>
2016-08-02 08:50:25 +0200

    Update RouterFacade.java

--- src/main/java/com/github/helpermethod/membrane/plugin/RouterFacade.java
+++ src/main/java/com/github/helpermethod/membrane/plugin/RouterFacade.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 helpermethod
+ * Copyright 2016 Oliver Weiler
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
